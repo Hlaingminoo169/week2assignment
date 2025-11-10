af072bd9ff498434f7e02bfc6e8e67f301a0a65e
Hlaingminoo169 <hlaingminoo785@gmail.com>
2025-11-10 10:19:58 +0630

    Initial project setup and files

---
+++ src/main.ts
@@ -0,0 +1,12 @@
+import Express = require("express");
+
+const app = Express();
+const port = 8081;
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
